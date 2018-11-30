2038d61c2edd5c2254973cbe4ff6922ffa054c3b
cyprian <cnwachukwu5@gmail.com>
2018-11-29 20:41:47 -0500

    FeedBack loop implemented

--- src/main/java/FeedBackLoop.java
+++ src/main/java/FeedBackLoop.java
@@ -1,11 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class FeedBackLoop {
 
-    private String[] nodeStatus = {"HIGH_SEND", "MODERATE_SEND", "LOW", "IDLE"};
+    private String[] nodeStatus = null;
+    private Map<String, Integer> stream_frequency = new HashMap<>();  //Simulate network delay based on oT status
 
-    public String currentNodeStatus(int index){
-        return nodeStatus[index];
+    public FeedBackLoop() {
+        this.nodeStatus = new String[] {"HIGH_SEND", "MODERATE_SEND", "LOW", "IDLE"};
+        this.stream_frequency.put("HIGH_SEND",1000);
+        this.stream_frequency.put("MODERATE_SEND",2000);
+        this.stream_frequency.put("LOW",5000);
+        this.stream_frequency.put("IDLE",60000);
+    }
+
+    public String currentNodeStatus(){
+        return nodeStatus[getIndex()];
     }
 
     private int getIndex(){
@@ -18,6 +29,10 @@ public class FeedBackLoop {
         Random rand = new Random();
 
         //Using (max-min+1) + 1 -- between 100 - 1000 milliseconds
-        return rand.nextInt(1000-100+1) + 1;
+        return rand.nextInt(10000-100+1) + 1;
+    }
+
+    public int stream_sending_frequency(String status){
+        return this.stream_frequency.get(status);
     }
 }
